e1d55285316c5fd0b76adec5e75a8e3c13f4f445
MaheedharTalluri <58215374+MaheedharTalluri@users.noreply.github.com>
2022-01-18 07:19:36 +0530

    Create docker-k8s-install.sh

---
+++ docker-k8s-install.sh
@@ -0,0 +1,15 @@
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+sudo yum install docker -y
+
+sudo service docker restart
+
+sudo usermod -aG docker $USER && newgrp docker
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+minikube start
